d30ec99b77ff87ccca8d57678d4f3e3885dd4c3b
Mille Marcoleta <131770818+marcoleta4@users.noreply.github.com>
2023-08-08 15:10:42 -0400

    Delete practica_final_orm_django directory

--- practica_final_orm_django/laboratorio/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Laboratorio, DirectorGeneral, Producto
-
-admin.site.register(Laboratorio)
-admin.site.register(DirectorGeneral)
-admin.site.register(Producto)
--- practica_final_orm_django/laboratorio/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Laboratorio
-
-class LaboratorioForm(forms.ModelForm):
-    class Meta:
-        model = Laboratorio
-        fields = ['nombre', 'ciudad', 'pais']
